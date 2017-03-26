09ef7fbebff5c96b3f4c6a293984f505a90da8fc
joferjoe <joferjoe@hotmail.com>
2017-03-26 09:24:30 +0100

    Deleted some stuff
    sorry :)

--- Assets/uMMO/Demos/BonusScenes/Scripts/uMMO_SelectCamera.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-using System.Collections;
-
-public class uMMO_SelectCamera : MonoBehaviour {
-
-    void __uMMO_localPlayer_init() {
-        Camera.main.gameObject.GetComponent<SoftRare.Div.ThirdPersonCamera>().follow = this.transform;
-    }
-}
