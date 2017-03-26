61ea11906c9baf2a65e3637f85ade3fb07c336f0
joferjoe <joferjoe@hotmail.com>
2017-03-26 16:23:11 +0100

    Starting shark
    Just noticed the player animations are swapping incorrectly, ill go back
    to it late :)

--- Assets/NETPLAYERS/PlayerScript.cs
+++ Assets/NETPLAYERS/PlayerScript.cs
@@ -7,6 +7,10 @@ public class PlayerScript : MonoBehaviour {
     public Animator ani;
     public Rigidbody rig;
 
+    float Timer = 0;
+    float MaxTimer = 1;
+    bool isMoving = false;
+
     // Use this for initialization
     void Start () {
         ani = GetComponent<Animator>();
@@ -17,15 +21,29 @@ public class PlayerScript : MonoBehaviour {
     Quaternion targetRotation;
     // Update is called once per frame
     void Update () {
+        Timer += Time.deltaTime;
         if (Input.GetMouseButtonDown(1))
         {
+            Timer = 0;
+            isMoving = true;
+            //ani.SetBool("IsMoving", true);
             Vector3 direction = Camera.main.transform.forward * 15;
 
             targetRotation = Quaternion.LookRotation(direction.normalized);
             targetRotation *= Quaternion.Euler(90, 0, 0);
             rig.AddForce(direction);
         }
+        if (Timer < MaxTimer)
+        {
+            isMoving = true;
+        }
+        else
+        {
+            isMoving = false;
+        }
 
+        ani.SetBool("isMoving", true);
+        
         rig.MoveRotation(transform.rotation = Quaternion.RotateTowards(rig.rotation, targetRotation, speed * Time.deltaTime));
     }
 
