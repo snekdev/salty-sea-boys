b650e25d13b9840b6b4cf78e937671b10d50a975
joferjoe <joferjoe@hotmail.com>
2017-03-26 22:58:52 +0100

    Getting closer :)

--- Assets/NETPLAYERS/SharkAi.cs
+++ Assets/NETPLAYERS/SharkAi.cs
@@ -4,13 +4,60 @@ using UnityEngine;
 
 public class SharkAi : MonoBehaviour {
     //List<List<string>> myList = new List<List<string>>();
+    public SharkPathFinding myPathFinding;
+    Rigidbody thisRigid;
+
+    bool isHunting = false;
+
+    public Transform targetPosition;
+
+    Quaternion targetRotation;
+    Vector3 Direction = Vector3.zero;
+    float speed = 50;
+
     // Use this for initialization
     void Start () {
-		
-	}
-	
+        thisRigid = GetComponent<Rigidbody>();
+        //targetPosition.position = new Vector3(10, 0, 0);
+
+
+    }
+    float moveSpeed = 0.1f;
 	// Update is called once per frame
 	void Update () {
-		
+
+        Direction = targetPosition.position - transform.position;
+
+        targetRotation = Quaternion.LookRotation(Direction.normalized);
+        targetRotation *= Quaternion.Euler(0, -90, 0);
+
+        PathToNextNode();
+
+        float distanceToTargetNode = Vector3.Distance(transform.position, targetPosition.position);
+        if (distanceToTargetNode < 1)
+        {
+            //PathToNextNode();
+        }
+        else
+        {
+            thisRigid.AddForce(Direction * moveSpeed);
+            thisRigid.MoveRotation(transform.rotation = Quaternion.RotateTowards(thisRigid.rotation, targetRotation, speed * Time.deltaTime));
+        }
+
+
 	}
+
+    void PathToNextNode()
+    {
+        //myPathFinding.FindNewOptimalPath(transform.position, targetPosition.position);
+        targetPosition.position = myPathFinding.FindNewOptimalPath(transform.position, targetPosition.position).position;
+    }
+    void __uMMO_serverNPO_init()
+    {
+        //thisAnimator.SetBool("IsMoving", true);
+        Debug.Log("init here");
+        Camera.main.GetComponent<ThirdPersonCamera.CameraController>().target = this.transform;
+
+        //PathToNextNode();
+    }
 }
--- Assets/NETPLAYERS/SharkPathFinding.cs
+++ Assets/NETPLAYERS/SharkPathFinding.cs
@@ -14,9 +14,60 @@ public class SharkPathFinding : MonoBehaviour {
 	void Update () {
 		
 	}
-
-    void FindNewOptimalPath()
+    
+    public Transform FindNewOptimalPath(Vector3 startLocation, Vector3 endLocation)
     {
 
+        return findStartingNode(startLocation);
+    }
+    Transform findStartingNode(Vector3 startLocation)
+    {
+        //SharkPathNode closestChild = null;
+        //float lowestDistance = float.MaxValue;
+        //foreach (Transform child in transform)
+        //{
+        //    float distance = Mathf.Abs(Vector3.Distance(startLocation, child.position));
+
+        //    if (distance < lowestDistance)
+        //    {
+        //        closestChild = child.GetComponent<SharkPathNode>();
+        //    }
+        //    // do whatever you want with child transform object here
+        //}
+        //return closestChild;
+
+
+
+
+        //SharkPathNode tMin = null;
+        //float minDist = Mathf.Infinity;
+        //Vector3 currentPos = transform.position;
+        //foreach (Transform t in transform)
+        //{
+        //    float dist = Vector3.Distance(t.position, currentPos);
+        //    if (dist < minDist)
+        //    {
+        //        tMin = t.GetComponent< SharkPathNode>();
+        //        minDist = dist;
+        //    }
+        //}
+        //return tMin;
+
+
+        Transform bestTarget = null;
+        float closestDistanceSqr = Mathf.Infinity;
+        Vector3 currentPosition = startLocation;
+        foreach (Transform potentialTarget in transform)
+        {
+            Vector3 directionToTarget = potentialTarget.position - currentPosition;
+            float dSqrToTarget = directionToTarget.sqrMagnitude;
+            if (dSqrToTarget < closestDistanceSqr)
+            {
+                closestDistanceSqr = dSqrToTarget;
+                bestTarget = potentialTarget;
+            }
+        }
+
+        return bestTarget;
     }
 }
