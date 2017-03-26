acbbcac5e2c9615360cec48b32b25e24b055b184
joferjoe <joferjoe@hotmail.com>
2017-03-26 10:20:52 +0100

    Camera and move
    Basic Camera functionality and movement test (my fish is facing the
    wrong way) haha

--- Assets/Fish/PlayerScript.cs
+++ Assets/Fish/PlayerScript.cs
@@ -6,6 +6,7 @@ public class PlayerScript : MonoBehaviour {
 
     public Animator ani;
     public Rigidbody rig;
+
     // Use this for initialization
     void Start () {
         ani = GetComponent<Animator>();
@@ -18,20 +19,21 @@ public class PlayerScript : MonoBehaviour {
         if (Input.GetMouseButtonDown(0))
         {
             ani.SetInteger("SwimState", 1);
-            rig.AddForce(Vector3.forward);
+            rig.AddForce(Vector3.forward * 100);
 
         }
 
-        if (Input.GetMouseButtonDown(1))
-        {
-            ani.SetInteger("SwimState", 2);
-            rig.AddForce(Vector3.forward);
-        }
+        //if (Input.GetMouseButtonDown(1))
+        //{
+        //    ani.SetInteger("SwimState", 2);
+        //    rig.AddForce(Vector3.forward * 100);
+        //}
 
     }
 
     void __uMMO_localPlayer_init()
     {
         Debug.Log("init here");
+        Camera.main.GetComponent<ThirdPersonCamera.CameraController>().target = this.transform;
     }
 }
