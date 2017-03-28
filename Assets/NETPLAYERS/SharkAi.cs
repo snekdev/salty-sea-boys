97ada0309d6d107ba3a2a32068772148d901f35b
joferjoe <joferjoe@hotmail.com>
2017-03-28 05:02:39 +0100

    shark works apart from for dealing damage :)

--- Assets/NETPLAYERS/SharkAi.cs
+++ Assets/NETPLAYERS/SharkAi.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class SharkAi : MonoBehaviour {
     //List<List<string>> myList = new List<List<string>>();
     public SharkPathFinding myPathFinding;
+    Animator thisAnimator;
     Rigidbody thisRigid;
 
     bool isHunting = false;
@@ -23,80 +24,139 @@ public class SharkAi : MonoBehaviour {
     // Use this for initialization
     void Start () {
         thisRigid = GetComponent<Rigidbody>();
+        thisAnimator = GetComponent<Animator>();
         //targetPosition.position = new Vector3(10, 0, 0);
 
 
         waterTransform = GameObject.FindGameObjectsWithTag("Water")[0];
     }
     float moveSpeed = 0.1f;
-	// Update is called once per frame
-	void Update () {
+    bool isPlayerCloseEnoughForLockOn = false;
+    float distanceFromPlayer = 999999;
 
-        if (sharkEyes.isVisible == true)
-        {
-            timeSinceLastSeen = 30;
-        }
-        if (timeSinceLastSeen > 0)
-        {
-            timeSinceLastSeen -= Time.deltaTime;
-            isHunting = true;
-        }
-        else
-        {
-            timeSinceLastSeen = 0;
-            isHunting = false;
-        }
+    // Update is called once per frame
+    void Update () {
 
-        if (isHunting == true)
+        if (sharkEyes.PlayerName.Length > 0)
         {
-            targetPosition.position = sharkEyes.LastSeenAt;
+           
 
-            float distanceToTargetLastKnownPosition = Vector3.Distance(transform.position, targetPosition.position);
-            if (distanceToTargetLastKnownPosition < 5)
+            GameObject playerObj = GameObject.Find(sharkEyes.PlayerName);
+
+            Direction = playerObj.transform.position - transform.position;
+
+            distanceFromPlayer = Vector3.Distance(transform.position, playerObj.transform.position);
+            if (distanceFromPlayer < 50)
             {
-                targetRotation.eulerAngles = new Vector3(this.transform.eulerAngles.x + (Mathf.Sin(Time.deltaTime) * 50), this.transform.eulerAngles.y, this.transform.eulerAngles.z + (Mathf.Sin(Time.deltaTime) * 50));
-
-                //thisRigid.AddForce(Direction * moveSpeed * 1.5f);
-                thisRigid.MoveRotation(transform.rotation = Quaternion.RotateTowards(thisRigid.rotation, targetRotation, speed * Time.deltaTime));
+                isPlayerCloseEnoughForLockOn = true;
             }
             else
             {
-                //move to player or last know position
-                //should probably raycast to look for them more accurately once identified
-                Direction = targetPosition.position - transform.position;
+                isPlayerCloseEnoughForLockOn = false;
+            }
 
 
+        }
+
+        if (isPlayerCloseEnoughForLockOn == true)
+        {
+            if (distanceFromPlayer < 10)
+            {
 
                 targetRotation = Quaternion.LookRotation(Direction.normalized);
-                targetRotation *= Quaternion.Euler(0, -90, 0);
+                thisAnimator.SetInteger("SwimState", 2);
+                thisRigid.AddForce(Direction * moveSpeed * 1.5f);
+                thisRigid.MoveRotation(transform.rotation = Quaternion.RotateTowards(thisRigid.rotation, targetRotation, speed * Time.deltaTime));
 
+                GameObject.Find(sharkEyes.PlayerName).GetComponent<PlayerScript>().PLAYERHEALTH -= 1;
+            }
+            else
+            {
+
+                targetRotation = Quaternion.LookRotation(Direction.normalized);
+                thisAnimator.SetInteger("SwimState", 1);
                 thisRigid.AddForce(Direction * moveSpeed * 1.5f);
                 thisRigid.MoveRotation(transform.rotation = Quaternion.RotateTowards(thisRigid.rotation, targetRotation, speed * Time.deltaTime));
             }
 
 
 
+
         }
         else
         {
-            Direction = targetPosition.position - transform.position;
-
-            targetRotation = Quaternion.LookRotation(Direction.normalized);
-            targetRotation *= Quaternion.Euler(0, -90, 0);
-
-            //PathToNextNode();
-
-            float distanceToTargetNode = Vector3.Distance(transform.position, targetPosition.position);
-            if (distanceToTargetNode < 5)
+            if (sharkEyes.isVisible == true)
             {
-                PathToNextNode();
+                timeSinceLastSeen = 30;
+            }
+            if (timeSinceLastSeen > 0)
+            {
+                timeSinceLastSeen -= Time.deltaTime;
+                isHunting = true;
             }
             else
             {
-                thisRigid.AddForce(Direction * moveSpeed);
-                thisRigid.MoveRotation(transform.rotation = Quaternion.RotateTowards(thisRigid.rotation, targetRotation, speed * Time.deltaTime));
+                timeSinceLastSeen = 0;
+                isHunting = false;
+            }
+
+            if (isHunting == true)
+            {
+                targetPosition.position = sharkEyes.LastSeenAt;
+
+                float distanceToTargetLastKnownPosition = Vector3.Distance(transform.position, targetPosition.position);
+                if (distanceToTargetLastKnownPosition < 5)
+                {
+                    targetRotation.eulerAngles = new Vector3(this.transform.eulerAngles.x + (Mathf.Sin(Time.deltaTime) * 50), this.transform.eulerAngles.y + (Mathf.Sin(Time.deltaTime) * 30), this.transform.eulerAngles.z + (Mathf.Sin(Time.deltaTime) * 70));
+
+                    //thisRigid.AddForce(Direction * moveSpeed * 1.5f);
+                    thisRigid.MoveRotation(transform.rotation = Quaternion.RotateTowards(thisRigid.rotation, targetRotation, speed * Time.deltaTime));
+                    thisAnimator.SetInteger("SwimState", 0);
+                }
+                else
+                {
+                    //move to player or last know position
+                    //should probably raycast to look for them more accurately once identified
+                    Direction = targetPosition.position - transform.position;
+
+
+
+                    targetRotation = Quaternion.LookRotation(Direction.normalized);
+                    //targetRotation *= Quaternion.Euler(0, -90, 0);
+
+                    thisAnimator.SetInteger("SwimState", 1);
+                    thisRigid.AddForce(Direction * moveSpeed * 1.5f);
+                    thisRigid.MoveRotation(transform.rotation = Quaternion.RotateTowards(thisRigid.rotation, targetRotation, speed * Time.deltaTime));
+                }
+
+
+
+            }
+            else
+            {
+                Direction = targetPosition.position - transform.position;
+
+                targetRotation = Quaternion.LookRotation(Direction.normalized);
+                //targetRotation *= Quaternion.Euler(0, -90, 0);
+
+                //PathToNextNode();
+
+                float distanceToTargetNode = Vector3.Distance(transform.position, targetPosition.position);
+                if (distanceToTargetNode < 5)
+                {
+                    PathToNextNode();
+                    thisAnimator.SetInteger("SwimState", 0);
+                }
+                else
+                {
+                    thisRigid.AddForce(Direction * moveSpeed);
+                    thisRigid.MoveRotation(transform.rotation = Quaternion.RotateTowards(thisRigid.rotation, targetRotation, speed * Time.deltaTime));
+                    thisAnimator.SetInteger("SwimState", 1);
+                }
             }
         }
+        
+
 
 
         if (this.transform.position.y > waterTransform.transform.position.y)
