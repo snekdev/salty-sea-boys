d64fdb3fb3e6097074298aeb96569fd5b2d8a39c
joferjoe <joferjoe@hotmail.com>
2017-03-28 15:54:51 +0100

    submitting while it still works GOD DAMMIT IT FISHES SPAWN

--- Assets/FishSpawnerNetwork.cs
+++ Assets/FishSpawnerNetwork.cs
@@ -4,43 +4,53 @@ using UnityEngine;
 using UnityEngine.Networking;
 
 public class FishSpawnerNetwork : NetworkBehaviour {
-    public GameObject fishObject;
-	// Use this for initialization
-	void Start () {
-		
-	}
-    float timer = 0;
+    public GameObject babyObject;
+
+    [SyncVar]
+    public bool ProduceChild;
+    [SyncVar]
+    public Vector3 VaginaLocation;
+
+    bool isServer = false;
+    // Use this for initialization
+    public override void OnStartServer()
+    {
+        // create and spawn
+        isServer = true;
+    }
+
+
+float timer = 0;
     float maxTimer = 3;
     // Update is called once per frame
     void Update()
     {
-        //timer += Time.deltaTime;
-        //if (timer >= maxTimer)
-        //{
-            //timer = 0;
-            //DoFire();
-        //}
-        //if (Input.GetKeyDown(KeyCode.Space))
-        //{
-        //    CmdDoFire();
-        //}
+        //if (!Network.isServer)
+        //return;
+        if (isServer == false)
+            return;
+
+        //PooOutBaby(VaginaLocation);
+        if (ProduceChild == true)
+        {
+            PooOutBaby(VaginaLocation);
+        }
     }
-    public void DoFire()
+    void PooOutBaby(Vector3 Vagina)
     {
-        Debug.Log("TEST");
-        GameObject tempHolder = Instantiate(fishObject, Vector3.zero, Quaternion.identity) as GameObject;
+        GameObject babyHolder = Instantiate(babyObject, Vector3.zero, Quaternion.identity) as GameObject;
+        NetworkServer.Spawn(babyHolder);
 
-
-        NetworkServer.Spawn(tempHolder);
+        ProduceChild = false;
     }
 
-    [Command]
-    void CmdDoFire()
-    {
-        Debug.Log("TEST");
-        GameObject tempHolder = Instantiate(fishObject, Vector3.zero,Quaternion.identity) as GameObject;
+    //[Command]
+    //void CmdDoFire()
+    //{
+    //    Debug.Log("TEST");
+    //    GameObject tempHolder = Instantiate(babyObject, Vector3.zero,Quaternion.identity) as GameObject;
 
 
-        NetworkServer.Spawn(tempHolder);
-    }
+    //    NetworkServer.Spawn(tempHolder);
+    //}
 }
---
+++ Assets/NETPLAYERS/testSpawnOnTouch.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class testSpawnOnTouch : NetworkBehaviour
+{
+    GameObject FishSpawner;
+    GameObject ParentPlayer;
+    PlayerScript myPlayerScript;
+
+    // Use this for initialization
+    void Start () {
+        FishSpawner = GameObject.FindGameObjectWithTag("FishManager");
+        ParentPlayer = transform.parent.gameObject;
+        myPlayerScript = ParentPlayer.GetComponent<PlayerScript>();
+            }
+	
+	// Update is called once per frame
+	void Update () {
+        if (myPlayerScript.gestation >= 5)
+        {
+            //myPlayerScript.gestation = 0;
+            FishSpawner.GetComponent<FishSpawnerNetwork>().ProduceChild = true;
+            FishSpawner.GetComponent<FishSpawnerNetwork>().VaginaLocation = this.transform.position;
+
+        }
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        //Destroy(other.gameObject);
+
+    }
+}
